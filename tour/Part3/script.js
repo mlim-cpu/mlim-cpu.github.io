d3adf12248868f452d9de595cf441bbeb177d9a9
mlim-cpu <limm806@newschool.edu>
2021-04-02 07:55:45 -0400

    .
    .

--- tour/Part3/script.js
+++ tour/Part3/script.js
@@ -3,3 +3,16 @@ function play() {
 var audio = document.getElementById("audio")
 audio.play();
 }
+
+function scramble() {
+  document.getElementById("ct").style.display = 'none';
+	document.getElementById("t").style.display = 'none';
+}
+
+function smoke() {
+var e = document.getElementById("cloud");
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+}
